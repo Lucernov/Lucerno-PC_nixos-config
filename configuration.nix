da4d88ef4ec7d4357c6af3c8e270896e0ca02578
Lucernov <jin.riv@gmail.com>
2026-05-04 17:34:35 +0300

    Автосинхронизация \2026-\05-\04 \17:\34:\35

--- configuration.nix
+++ configuration.nix
@@ -48,6 +48,8 @@ in
   };
   hardware.ksm.enable = false;
   musnix.enable = true;
+  musnix.snd_hda_intel.enable = false;
+  musnix.kernel.realtime = false;       # Оставляем false для совместимости с драйверами NVIDIA
 
 
   # ========== ДОПОЛНИТЕЛЬНЫЕ ДИСКИ ==========
