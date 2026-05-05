b994665d744925792634debffd303928d3ae57e2
Lucernov <jin.riv@gmail.com>
2026-05-05 22:46:20 +0300

    pre-rebuild

--- configuration.nix
+++ configuration.nix
@@ -117,6 +117,8 @@
     GDK_BACKEND = "wayland";
     SDL_VIDEODRIVER = "wayland";
     QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt6.qtwayland}/lib/qt-6/plugins/platforms";
+    #LD_LIBRARY_PATH = "/run/current-system/sw/lib";
+    #QT_PLUGIN_PATH = "/run/current-system/sw/lib/qt-6/plugins";
   };
 
   # ========== Звук (PipeWire) ==========
@@ -155,7 +157,10 @@
 
 systemd.user.services.plasma-plasmashell = {
   overrideStrategy = "asDropin";
-  serviceConfig.LimitNOFILE = 16384;
+  serviceConfig = {
+    LimitNOFILE = 16384;
+    Environment = "PATH=/run/current-system/sw/bin:/etc/profiles/per-user/lucerno/bin:/nix/var/nix/profiles/default/bin:/home/lucerno/.local/bin";
+  };
 };
 systemd.user.services.kwin_wayland = {
   serviceConfig.LimitNOFILE = 16384;
