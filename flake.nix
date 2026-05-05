430564f6e70c35c3396b3e2d4fa6c11d52ad8d8d
Lucernov <jin.riv@gmail.com>
2026-05-06 00:07:05 +0300

    pre-rebuild

--- flake.nix
+++ flake.nix
@@ -25,30 +25,9 @@
   outputs = inputs@{ flake-parts, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, musnix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-
-      imports = [
-        # здесь можно будет добавить другие модули flake-parts (например, для home-manager)
-      ];
-
+      imports = [ ];
       flake = {
-        # homeConfigurations остаётся как есть (вне nixosConfigurations)
-        homeConfigurations.lucerno = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [ ./home.nix ];
-          extraSpecialArgs = {
-            inherit inputs;
-            pkgs-unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
-          };
-        };
-      };
-
-      perSystem = { config, pkgs, ... }: {
-        # пока пусто, но можно добавить общие пакеты для devShell и т.д.
-      };
-
-      # ВАЖНО: nixosConfigurations определяется ТАК
-      nixosConfigurations = {
-        Lucerno-PC = nixpkgs.lib.nixosSystem {
+        nixosConfigurations.Lucerno-PC = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = {
             inherit inputs;
@@ -58,13 +37,8 @@
             };
           };
           modules = [
-            # твой новый модуль хоста (именно он импортирует старые конфиги)
             ./modules/hosts/Lucerno-PC
-
-            # musnix
             musnix.nixosModules.musnix
-
-            # home-manager как модуль NixOS (для бесшовной интеграции)
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
@@ -79,6 +53,15 @@
             }
           ];
         };
+        homeConfigurations.lucerno = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./home.nix ];
+          extraSpecialArgs = {
+            inherit inputs;
+            pkgs-unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+          };
+        };
       };
+      perSystem = { config, pkgs, ... }: { };
     };
 }
