52a7950614cf61d58bef9d758eb16a8aa063e74a
Lucernov <jin.riv@gmail.com>
2026-05-05 23:01:36 +0300

    pre-rebuild

--- configuration.nix
+++ configuration.nix
@@ -159,15 +159,12 @@ systemd.user.services.plasma-plasmashell = {
   overrideStrategy = "asDropin";
   serviceConfig = {
     LimitNOFILE = 16384;
+    Environment = "PATH=/run/current-system/sw/bin:/etc/profiles/per-user/lucerno/bin:/nix/var/nix/profiles/default/bin:/home/lucerno/.local/bin";
   };
 };
 systemd.user.services.kwin_wayland = {
   serviceConfig.LimitNOFILE = 16384;
 };
-
-  systemd.user.extraConfig = ''
-    DefaultEnvironment=PATH=/run/current-system/sw/bin:/etc/profiles/per-user/lucerno/bin:/nix/var/nix/profiles/default/bin
-  '';
 
   # ========== STEAM ==========
   programs.steam = {
