b8aad6b67cab3fb7d4dfe699bd1bbbbbbdb7ad3e
Lucernov <jin.riv@gmail.com>
2026-05-06 14:31:30 +0300

    pre-rebuild

--- modules/nixos/profiles/sddm.nix
+++ modules/nixos/profiles/sddm.nix
@@ -2,14 +2,11 @@
 
 {
   services.displayManager.sddm = {
-    enable = true;                                                # Включает SDDM (Simple Desktop Display Manager)
-    wayland.enable = true;                                        # Разрешает SDDM работать под Wayland
+    enable = true;
+    wayland.enable = true;
   };
-  #services.displayManager.plasma-login-manager.enable = true;    # Plasma Login Manager (PLM) — это новый менеджер входа
+  #services.displayManager.plasma-login-manager.enable = true;
 
-  # ПОДМЕНА ФОНА SDDM
-  # файл theme.conf.user внутри темы breeze. SDDM читает этот файл и применяет настройки, не затрагивая оригинальные файлы темы.
-  # Параметр background указывает на пакет mySddmBackground
   environment.systemPackages = [
     (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
       [General]
@@ -17,9 +14,8 @@
     '')
   ];
 
-    # ========== Переменные окружения для менеджера входа ==========
   environment.sessionVariables = {
     LANG = "ru_RU.UTF-8";
     LANGUAGE = "ru_RU.UTF-8";
+  };
 }
-
