3752a10390d94b56144ea7a6c3d77090e830f031
Lucernov <jin.riv@gmail.com>
2026-05-07 04:04:05 +0300

    pre-rebuild

--- modules/hosts/Lucerno-PC/default.nix
+++ modules/hosts/Lucerno-PC/default.nix
@@ -7,19 +7,6 @@
   imports = (import-tree ../../../modules/nixos/profiles) ++ [
     ../../../nixos/hardware-configuration.nix
     ../../../nixos/hardware.nix
-
-    # новые профили
-    #../../../modules/nixos/profiles/locale.nix
-    #../../../modules/nixos/profiles/users.nix
-    #../../../modules/nixos/profiles/sddm.nix
-    #../../../modules/nixos/profiles/firewall.nix
-    #../../../modules/nixos/profiles/environment.nix
-    #../../../modules/nixos/profiles/pipewire.nix
-    #../../../modules/nixos/profiles/systemd-limits.nix
-    #../../../modules/nixos/profiles/steam.nix
-    #../../../modules/nixos/profiles/nix-optimization.nix
-    #../../../modules/nixos/profiles/configuration-kde_plasma.nix
-    #../../../modules/nixos/profiles/soft-sys.nix
   ];
 
   # ========== мои симлинки ==========
