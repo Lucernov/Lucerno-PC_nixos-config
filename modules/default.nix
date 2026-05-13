d6ee349118a161e78aecc4acd18ba1fbc41afae6
Lucernov <jin.riv@gmail.com>
2026-05-13 21:07:35 +0300

    pre-rebuild

--- modules/default.nix
+++ modules/default.nix
@@ -30,11 +30,6 @@
   boot.supportedFilesystems = [ "exfat" ];
   #system.nixos-init.enable = true;                        # иногда проверять, пока проблемы с нвидиа
 
-  systemd.services.systemd-tmpfiles-setup = {
-    after = [ "userborn.service" "home.mount" ];
-    requires = [ "userborn.service" ];
-  };
-
   # ========== Аудио оптимизация (musnix) ==========
   musnix.enable = true;
   musnix.kernel.realtime = false;                          # для совместимости с NVIDIA
