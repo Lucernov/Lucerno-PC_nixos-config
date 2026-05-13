c090dc4b7f1badc546c4b8b5ed9094435ac92228
Lucernov <jin.riv@gmail.com>
2026-05-13 20:46:44 +0300

    pre-rebuild

--- modules/default.nix
+++ modules/default.nix
@@ -32,6 +32,14 @@
   boot.initrd.systemd.enable = true;
   system.etc.overlay.enable = true;
   services.userborn.enable = true;
+    systemd.services.userborn = {
+    after = [ "home.mount" "local-fs.target" ];
+    wants = [ "home.mount" ];
+    };
+    systemd.services.systemd-tmpfiles-setup = {
+    after = [ "userborn.service" ];
+    requires = [ "userborn.service" ];
+    };
   system.nixos-init.enable = true;                        # иногда проверять, пока проблемы с нвидиа
 
   # ========== Аудио оптимизация (musnix) ==========
