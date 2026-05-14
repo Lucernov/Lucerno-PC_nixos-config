f5bc8edb048637ed32b01ab7c309e5a825d86860
Lucernov <jin.riv@gmail.com>
2026-05-14 18:19:45 +0300

    pre-rebuild

--- modules/nx_users.nix
+++ modules/nx_users.nix
@@ -7,13 +7,7 @@
     isNormalUser = true;                                                             # Обычный пользователь (не системный)
     hashedPasswordFile = "/home/lucerno/nixos-config/secrets/lucerno-password.hash"; # Файл с хешем пароля
     group = "lucerno";                                                         # Группа, к которой принадлежит пользователь
-    # Дополнительные группы
-    # wheel   – право выполнять команды через sudo
-    # networkmanager – управление сетью
-    # audio   – доступ к звуковым устройствам
-    # video   – доступ к видеодрайверу (GPU)
-    # storage – доступ к дискам и файловым системам
-    extraGroups = [ "wheel" "networkmanager" "audio" "video" "storage" ];
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "storage" "render" ];
     shell = pkgs.zsh;                                                                # Командная оболочка по умолчанию (Zsh)
   };
 
