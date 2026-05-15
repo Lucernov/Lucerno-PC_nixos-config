0cc04691dd3cd8855064ce3af9dfa6cbc2a897e6
Lucernov <jin.riv@gmail.com>
2026-05-16 01:27:14 +0300

    upgrade: Сб 16 мая 2026 01:27:14 MSK

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,4 @@
   minion = pkgs.callPackage ./minion.nix { };
   qmmp = pkgs.callPackage ./qmmp.nix { };
   reaper = pkgs.callPackage ./reaper.nix { reaper = pkgs-unstable.reaper; };
-  krita-ai = pkgs.callPackage ./krita-ai.nix { };
 }
