60df083755e13ec52fe7ae01ac63a990531c18b6
Lucernov <jin.riv@gmail.com>
2026-05-17 11:14:08 +0300

    pre-rebuild

--- pkgs/reaper.nix
+++ pkgs/reaper.nix
@@ -1,6 +1,13 @@
 # pkgs/reaper.nix
-{ writeShellScriptBin, reaper }:
-writeShellScriptBin "reaper" ''
-  export GDK_BACKEND=x11
-  exec taskset -c 2-11 ${reaper}/bin/reaper "$@"
-''
+{ symlinkJoin, makeWrapper, reaper, util-linux }:
+symlinkJoin {
+  name = "reaper-wrapped";
+  paths = [ reaper ];
+  buildInputs = [ makeWrapper ];
+  postBuild = ''
+    # Создаём обёртку, которая устанавливает переменные окружения и запускает reaper
+    makeWrapper ${reaper}/bin/reaper $out/bin/reaper \
+      --set GDK_BACKEND x11 \
+      --run "exec ${util-linux}/bin/taskset -c 2-11"
+  '';
+}
