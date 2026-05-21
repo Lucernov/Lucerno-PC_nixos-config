8ea9eddddfb96c4c802cec6ef0e44f28e33079ea
Lucernov <jin.riv@gmail.com>
2026-05-21 10:14:28 +0300

    hm: 2026-05-21 10:14:28

--- modules/hx_kitty.nix
+++ modules/hx_kitty.nix
@@ -35,10 +35,10 @@
       "ctrl+ц" = "close_tab";                    # То же для русской раскладки (буква 'ц')
       "ctrl+right" = "next_tab";                 # Ctrl+Right – следующая вкладка
       "ctrl+left" = "previous_tab";              # Ctrl+Left – предыдущая вкладка
-      "ctrl+shift+e" = "launch --location=hsplit";  # Разделение окна по горизонтали (верх/низ)
-      "ctrl+shift+у" = "launch --location=hsplit";  # Разделение окна по горизонтали (верх/низ)
-      "ctrl+shift+d" = "launch --location=vsplit";  # Разделение окна по вертикали (лево/право)
-      "ctrl+shift+в" = "launch --location=vsplit";  # Разделение окна по вертикали (лево/право)
+      "ctrl+alt+e" = "launch --location=hsplit";  # Разделение окна по горизонтали (верх/низ)
+      "ctrl+alt+у" = "launch --location=hsplit";  # Разделение окна по горизонтали (верх/низ)
+      "ctrl+alt+d" = "launch --location=vsplit";  # Разделение окна по вертикали (лево/право)
+      "ctrl+alt+в" = "launch --location=vsplit";  # Разделение окна по вертикали (лево/право)
     };
 
     # Любые другие строки, которые не поддерживаются settings/keybindings
