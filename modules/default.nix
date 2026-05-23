dde749e084aa71a96eed0bfdabb48754d9ebdf69
Lucernov <jin.riv@gmail.com>
2026-05-23 14:53:34 +0300

    pre-rebuild

--- modules/default.nix
+++ modules/default.nix
@@ -5,7 +5,8 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "25.11";
 
-   services.displayManager.generic = lib.mkIf false { };
+  services.displayManager.generic = { };
+
 
   imports = [
     ../hardware-configuration.nix
