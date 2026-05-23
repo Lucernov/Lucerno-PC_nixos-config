00f6194464282721403eb62ac918a56c2472ec6d
Lucernov <jin.riv@gmail.com>
2026-05-23 14:32:40 +0300

    pre-rebuild

--- modules/nx_stylix.nix
+++ modules/nx_stylix.nix
@@ -8,9 +8,5 @@ let myLib = import ../lib.nix; in
     image = myLib.wallpaperPath;
     polarity = "dark";             # "light" или "dark"
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-    targets = {
-      generic.enable = true;      # Отключаем настройку generic display manager
-      sddm.enable = true;          # Включаем явную поддержку SDDM
-    };
   };
 }
