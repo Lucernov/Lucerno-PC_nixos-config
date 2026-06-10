df7406abd9c391c2b252c8e2d2b63ae96ecc4e0f
Lucernov <jin.riv@gmail.com>
2026-06-11 00:01:11 +0300

    hm: 2026-06-11 00:01:11

---
+++ modules/hx_btop.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  # Создаём симлинк на тему
+  home.file.".config/btop/themes/adapta.theme".source = ../dotfiles/config/btop/adapta.theme;
+
+  # Настраиваем btop декларативно
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "adapta";          # просто имя
+      save_config_on_exit = false;     # отключаем автосохранение (чтобы не перезаписывало)
+      # Здесь можно прописать любые другие настройки, которые вы хотите зафиксировать.
+      # Например, из вашего текущего btop.conf:
+      truecolor = true;
+      theme_background = true;
+      graph_symbol = "braille";
+      proc_sorting = "cpu direct";
+      # ... и так далее.
+    };
+  };
+}
