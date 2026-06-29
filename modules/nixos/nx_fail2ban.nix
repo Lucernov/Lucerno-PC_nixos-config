b2aa8de1fab858e66c4f70335ae33ad1be5146a7
Lucernov <jin.riv@gmail.com>
2026-06-29 14:17:02 +0300

    pre-rebuild

--- modules/nixos/nx_fail2ban.nix
+++ modules/nixos/nx_fail2ban.nix
@@ -3,19 +3,12 @@
 {
   services.fail2ban = {
     enable = true;
-    # Так как у вас включён nftables, указываем бэкенд
-    banaction = "nftables-multiport";
-    # Игнорируем локальные адреса (чтобы не забанить себя)
     ignoreIP = [
-        "127.0.0.1/8"     # localhost IPv4
-        "::1"             # localhost IPv6
-        "192.168.0.0/24"  # локальная сеть (если маска /24)
-        # "192.168.0.0/16"  # если вся сеть 192.168.*.*
+      "127.0.0.1/8"
+      "::1"
+      "192.168.0.0/24"
     ];
-
-    # Определяем джейлы (правила)
     jails = {
-      # SSH (основной)
       sshd = ''
         enabled = true
         port    = ssh
@@ -24,24 +17,17 @@
         maxretry = 3
         bantime  = 1h
         findtime = 10m
-        # Явно указываем таблицу, семейство и цепочку:
-        action  = nftables-multiport[table=nixos-fw, family=inet, chain=input]
+        action  = iptables-multiport[name=sshd, port=ssh, protocol=tcp]
       '';
-      # Можно добавить другие, например, для веб-сервера, если он есть
-      # nginx-http-auth = ''
-      #   enabled = true
-      #   port    = http,https
-      #   filter  = nginx-http-auth
-      #   logpath = /var/log/nginx/error.log
-      #   maxretry = 5
-      #   bantime  = 1d
-      # '';
+    };
+    extraConfig = ''
+      [DEFAULT]
+      banaction = iptables-multiport
+    '';
+  };
+}
 
 
 #sudo systemctl status fail2ban
 #sudo fail2ban-client status sshd
 #sudo nft list ruleset | grep -A5 "fail2ban"
-    };
-  };
-}
-
