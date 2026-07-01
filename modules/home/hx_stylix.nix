b7d404f99347f7638e1bd5e29bfe71ae7fc9792c
Lucernov <jin.riv@gmail.com>
2026-07-02 00:23:17 +0300

    pre-rebuild

--- modules/home/hx_stylix.nix
+++ modules/home/hx_stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    base16Scheme = "${inputs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${inputs.base16-schemes}/share/themes/nord.yaml";
     targets.kitty.enable = true;
     opacity.terminal = 0.80;
   };
--- modules/nixos/nx_stylix.nix
+++ modules/nixos/nx_stylix.nix
@@ -6,9 +6,9 @@
     stylix = {
       enable = true;                                                                      # Включает глобальное управление темами через stylix
       image = myLib.wallpaperPath;                                                        # Путь к изображению для обоев рабочего стола и экрана входа
-      polarity = "dark";                                                                  # Цветовая направленность: "dark" (тёмная) или "light" (светлая)
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";                     # Активная цветовая схема в формате base16 (Catppuccin Mocha)
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";         # Активная цветовая схема в формате base16 (Catppuccin Mocha)
+      #polarity = "dark";                                                                  # Цветовая направленность: "dark" (тёмная) или "light" (светлая)
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";                     # Активная цветовая схема в формате base16 (Catppuccin Mocha)
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";         # Активная цветовая схема в формате base16 (Catppuccin Mocha)
     };
   };
 }
