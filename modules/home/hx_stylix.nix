6e16bb5612c96966b19ac855cdd12be5846fa490
Lucernov <jin.riv@gmail.com>
2026-07-01 22:56:01 +0300

    hm: 2026-07-01 22:56:01

--- modules/home/hx_stylix.nix
+++ modules/home/hx_stylix.nix
@@ -1,9 +1,9 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   stylix = {
-    base16Scheme = "${inputs.stylix.inputs.tinted-schemes}/base16/nord.yaml";
-    targets.kitty.enable = true;                # явно включаем поддержку Kitty
-    opacity.terminal = 0.95;                    # уровень прозрачности терминала
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    targets.kitty.enable = true;
+    opacity.terminal = 0.95;
   };
 }
