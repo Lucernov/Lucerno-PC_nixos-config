526adc3df1a11ad05b83baf5905d0feb23aaf796
Lucernov <jin.riv@gmail.com>
2026-07-02 00:17:22 +0300

    hm: 2026-07-02 00:17:22

--- modules/home/hx_stylix.nix
+++ modules/home/hx_stylix.nix
@@ -2,7 +2,8 @@
 
 {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     targets.kitty.enable = true;
     opacity.terminal = 0.80;
   };
