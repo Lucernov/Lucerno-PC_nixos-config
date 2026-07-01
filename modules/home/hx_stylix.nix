12ce2909146984589fbf15f9eb03a0fae5c1b34a
Lucernov <jin.riv@gmail.com>
2026-07-01 22:45:53 +0300

    hm: 2026-07-01 22:45:53

--- modules/home/hx_stylix.nix
+++ modules/home/hx_stylix.nix
@@ -1,9 +1,10 @@
 { inputs, ... }:
 
 {
-  # Правильный путь к схеме Nord внутри репозитория tinted-schemes
-  stylix.base16Scheme = "${inputs.stylix.inputs.tinted-schemes}/base16/nord.yaml";
-
-  # Прозрачность фона терминала
-  stylix.opacity.terminal = 0.95;
+  stylix = {
+    base16Scheme = "${inputs.stylix.inputs.tinted-schemes}/base16/nord.yaml";
+    targets.kitty.enable = true;                # явно включаем поддержку Kitty
+    targets.kitty.transparentBackground = true; # разрешаем прозрачность фона
+    opacity.terminal = 0.95;                    # уровень прозрачности терминала
+  };
 }
