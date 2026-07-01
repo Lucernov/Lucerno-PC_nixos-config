db251907a96dfa5cb43e0c9bde87292e26c759ea
Lucernov <jin.riv@gmail.com>
2026-07-01 19:15:21 +0300

    hm: 2026-07-01 19:15:21

--- modules/home/hx_stylix.nix
+++ modules/home/hx_stylix.nix
@@ -2,13 +2,8 @@
 
 {
   # Указываем Stylix использовать схему Nord
-  # Мы берем её напрямую из зависимостей самого Stylix (там лежат все tinted-schemes)
   stylix.base16Scheme = "${inputs.stylix.inputs.tinted-schemes}/share/themes/nord.yaml";
 
-  # Возвращаем прозрачность фона терминала, которую ты убрал из настроек Kitty
-  # (по умолчанию Stylix делает терминал непрозрачным)
+  # Возвращаем прозрачность фона терминала
   stylix.opacity.terminal = 0.95;
-
-  # Опционально: если хочешь, чтобы курсор тоже был от Nord (а не дефолтный)
-  stylix.cursor.name = "breeze_snow"; # или любой другой установленный курсор
 }
