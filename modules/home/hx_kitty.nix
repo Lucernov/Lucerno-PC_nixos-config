3407866d5174a271803fefa56ba358c770f24e4e
Lucernov <jin.riv@gmail.com>
2026-07-01 23:18:12 +0300

    pre-rebuild

--- modules/home/hx_kitty.nix
+++ modules/home/hx_kitty.nix
@@ -1,9 +1,7 @@
 { pkgs, ... }:
 {
-  # ========== Kitty терминал ==========
   programs.kitty = {
-    enable = true;                                                                # Включает генерацию конфигурации Kitty
-    package = null;                                                               # Не устанавливаем Kitty через home-manager (пакет уже установлен системно)
+    enable = true;
+    package = null;
   };
-
 }
