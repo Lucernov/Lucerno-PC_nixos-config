afcfcaa58b97305a99d70743c102f06bceeef857
Lucernov <jin.riv@gmail.com>
2026-07-02 20:24:20 +0300

    upgrade: 2026-07-02 20:24:20

--- pkgs/pkg_socialstreamninja.nix
+++ pkgs/pkg_socialstreamninja.nix
@@ -1,33 +1,37 @@
 { stdenv, lib, appimageTools, fetchurl, makeWrapper }:
 
 let
-  # Версия приложения (можно обновлять при выходе новых версий)
   version = "0.3.128";
   pname = "socialstreamninja";
 
-  # URL для скачивания AppImage
   src = fetchurl {
     url = "https://github.com/steveseguin/social_stream/releases/download/v${version}/socialstreamninja_linux_v${version}_x86_64.AppImage";
-    # Хеш SHA-256 файла (вычисли командой: nix-prefetch-url <URL>)
-    sha256 = "sha256-16mlg0vxxb7v2bp42y8sphbmdppsp4s2dsam27sl9v996qzx5a10";
+    hash = "sha256-2uRfHEN19X6mpsPT0ffN6aX9vKhUSOhhxBywhsKnHpE=";
+  };
+
+  # Распаковываем AppImage
+  appimage = appimageTools.extract {
+    inherit pname version src;
   };
 
 in
-appimageTools.wrapType2 {
-  name = "socialstreamninja";
+stdenv.mkDerivation {
   inherit pname version src;
 
-  # Дополнительные зависимости, если нужны (обычно для AppImage хватает базовых)
-  extraPkgs = pkgs: with pkgs; [
-    # Например, если приложению нужны библиотеки, можно добавить:
-    # libglvnd
-    # alsa-lib
-  ];
+  buildInputs = [ makeWrapper ];
+
+  buildCommand = ''
+    mkdir -p $out/bin
+    # Копируем все файлы из распакованного AppImage в store
+    cp -r ${appimage}/* $out/
+    # Создаём обёртку, которая запускает AppRun с правильным окружением
+    makeWrapper $out/AppRun $out/bin/${pname}
+  '';
 
   meta = {
-    description = "Управление социальными сетями";
+    description = "Управление социальными сетями (AppImage)";
     homepage = "https://socialstreamninja.com";
-    license = lib.licenses.unfree; # или проприетарная
+    license = lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
     maintainers = with lib.maintainers; [ ];
   };
