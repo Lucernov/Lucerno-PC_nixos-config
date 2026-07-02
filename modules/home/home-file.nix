b6518bf1b9da26f1602e994ecec71432434671c4
Lucernov <jin.riv@gmail.com>
2026-07-02 20:41:17 +0300

    upgrade: 2026-07-02 20:41:17

--- modules/home/home-file.nix
+++ modules/home/home-file.nix
@@ -86,7 +86,7 @@
       Name=SocialStreamNinja
       Comment=Управление социальными сетями
     # Exec=/mnt/sys_archiv/pkgs/AppImages/socialstreamninja_linux_v0.3.113_x86_64.AppImage
-      Exec=${pkgs.my-packages.socialstreamninja}/bin/socialstreamninja %F
+      Exec=${pkgs.my-packages.socialstreamninja}/bin/socialstreamninja
       Icon=${myLib.home}/${myLib.configDirName}/dotfiles/sys-icons/icon-SocialStreamNinja.png
       Categories=Network;
       Terminal=false
--- pkgs/pkg_socialstreamninja.nix
+++ pkgs/pkg_socialstreamninja.nix
@@ -1,31 +1,18 @@
-{ stdenv, lib, appimageTools, fetchurl, makeWrapper }:
+{ lib, stdenv, fetchurl }:
 
-let
-  version = "0.3.128";
+stdenv.mkDerivation {
   pname = "socialstreamninja";
+  version = "0.3.128";
 
   src = fetchurl {
-    url = "https://github.com/steveseguin/social_stream/releases/download/v${version}/socialstreamninja_linux_v${version}_x86_64.AppImage";
+    url = "https://github.com/steveseguin/social_stream/releases/download/v0.3.128/socialstreamninja_linux_v0.3.128_x86_64.AppImage";
     hash = "sha256-IKjSPzYp7UT1EVXpJjS5+t5WF7waeUHuEvus3jd4tJo=";
   };
 
-  # Распаковываем AppImage
-  appimage = appimageTools.extract {
-    inherit pname version src;
-  };
-
-in
-stdenv.mkDerivation {
-  inherit pname version src;
-
-  buildInputs = [ makeWrapper ];
-
-  buildCommand = ''
+  installPhase = ''
     mkdir -p $out/bin
-    # Копируем все файлы из распакованного AppImage в store
-    cp -r ${appimage}/* $out/
-    # Создаём обёртку, которая запускает AppRun с правильным окружением
-    makeWrapper $out/AppRun $out/bin/${pname}
+    cp $src $out/bin/socialstreamninja
+    chmod +x $out/bin/socialstreamninja
   '';
 
   meta = {
