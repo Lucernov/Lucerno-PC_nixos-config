c62c5d3f782229b94bf778ce89bb7f2b9741cfa3
Lucernov <jin.riv@gmail.com>
2026-07-03 13:21:51 +0300

    pre-rebuild

--- modules/nixos/scripts.nix
+++ modules/nixos/scripts.nix
@@ -1,16 +1,32 @@
 # modules/scripts.nix
 { pkgs, ... }:
 
-{
-  environment.systemPackages = with pkgs; [
-    (writeShellScriptBin "video-player" ''
-      #!/usr/bin/env bash
-      CODEC=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$1" 2>/dev/null)
-      if [[ "$CODEC" == "av1" ]]; then
-          exec mpv --hwdec=nvdec "$@"
-      else
-          exec vlc "$@"
-      fi
-    '')
-  ];
+let
+  videoPlayerScript = pkgs.writeShellScriptBin "video-player" ''
+    #!/usr/bin/env bash
+    CODEC=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$1" 2>/dev/null)
+    if [[ "$CODEC" == "av1" ]]; then
+        exec mpv --hwdec=nvdec "$@"
+    else
+        exec vlc "$@"
+    fi
+  '';
+
+  # Создаём .desktop-файл как отдельный пакет
+  videoPlayerDesktop = pkgs.writeTextFile {
+    name = "video-player-desktop";
+    destination = "/share/applications/video-player.desktop";
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Video Player (AV1-aware)
+      Exec=video-player %F
+      MimeType=video/mp4;video/x-matroska;video/avi;video/quicktime;video/webm;
+      Icon=video-x-generic
+      Terminal=false
+      Categories=AudioVideo;Player;
+    '';
+  };
+in {
+  environment.systemPackages = [ videoPlayerScript videoPlayerDesktop ];
 }
