f49233646bf669393bac2cc6e6e38ef27440e503
Lucernov <jin.riv@gmail.com>
2026-07-05 23:06:36 +0300

    pre-rebuild

--- pkgs/pkg_minion.nix
+++ pkgs/pkg_minion.nix
@@ -1,11 +1,18 @@
-# pkgs/minion.nix
-{ symlinkJoin, makeWrapper, minion }:
+{ symlinkJoin, makeWrapper, minion, openjfx }:
 symlinkJoin {
   name = "minion-wrapped";
   paths = [ minion ];
   buildInputs = [ makeWrapper ];
   postBuild = ''
+    # Собираем CLASSPATH из JAR-файлов JavaFX
+    classpath=""
+    for jar in ${openjfx}/lib/*.jar; do
+      classpath="$classpath:$jar"
+    done
+    classpath="''${classpath#:}"
+
     wrapProgram $out/bin/minion \
-      --set JAVA_TOOL_OPTIONS "-Dprism.lcdtext=false -Dprism.text=t2k"
+      --set JAVA_TOOL_OPTIONS "-Dprism.lcdtext=false -Dprism.text=t2k" \
+      --set CLASSPATH "$classpath"
   '';
 }
