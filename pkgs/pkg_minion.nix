7a143c163457a3d5af53020b1ee1a8e358d9c34d
Lucernov <jin.riv@gmail.com>
2026-07-05 23:38:54 +0300

    pre-rebuild

--- pkgs/pkg_minion.nix
+++ pkgs/pkg_minion.nix
@@ -3,10 +3,10 @@ let
   javafxVersion = "21.0.3";
   javafxModules = [ "base" "controls" "fxml" "graphics" ];
   javafxJars = builtins.listToAttrs (map (m: {
-    name = "javafx-${m}";
+    name = m;
     value = fetchurl {
       url = "https://repo1.maven.org/maven2/org/openjfx/javafx-${m}/${javafxVersion}/javafx-${m}-${javafxVersion}.jar";
-      hash = "sha256-Em3a2XaQVhyEQAKf+FrDkRZc+9HKBkV+gRx8zOu2hoM=";
+      hash = if m == "graphics" then "sha256-Em3a2XaQVhyEQAKf+FrDkRZc+9HKBkV+gRx8zOu2hoM=" else "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
     };
   }) javafxModules);
 in
@@ -15,15 +15,20 @@ symlinkJoin {
   paths = [ minion ];
   buildInputs = [ makeWrapper ];
   postBuild = ''
-    # Собираем JAR-файлы JavaFX в одну папку
     mkdir -p $out/share/minion/javafx
-    ln -s ${javafxJars.javafx-base} $out/share/minion/javafx/javafx-base.jar
-    ln -s ${javafxJars.javafx-controls} $out/share/minion/javafx/javafx-controls.jar
-    ln -s ${javafxJars.javafx-fxml} $out/share/minion/javafx/javafx-fxml.jar
-    ln -s ${javafxJars.javafx-graphics} $out/share/minion/javafx/javafx-graphics.jar
+    cp ${javafxJars.base} $out/share/minion/javafx/javafx-base.jar
+    cp ${javafxJars.controls} $out/share/minion/javafx/javafx-controls.jar
+    cp ${javafxJars.fxml} $out/share/minion/javafx/javafx-fxml.jar
+    cp ${javafxJars.graphics} $out/share/minion/javafx/javafx-graphics.jar
+
+    classpath=""
+    for jar in $out/share/minion/javafx/*.jar $out/share/minion/lib/*.jar $out/share/minion/Minion-jfx.jar; do
+      classpath="$classpath:$jar"
+    done
+    classpath="''${classpath#:}"
 
     wrapProgram $out/bin/minion \
       --set JAVA_TOOL_OPTIONS "-Dprism.lcdtext=false -Dprism.text=t2k" \
-      --add-flags "-cp $out/share/minion/javafx/*:$out/share/minion/Minion-jfx.jar:$out/share/minion/lib/*"
+      --set CLASSPATH "$classpath"
   '';
 }
