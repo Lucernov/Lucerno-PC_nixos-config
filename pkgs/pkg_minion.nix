42c531fe981db4a09639e4d3afa9ab05039d67c9
Lucernov <jin.riv@gmail.com>
2026-07-06 14:48:55 +0300

    pre-rebuild

--- pkgs/pkg_minion.nix
+++ pkgs/pkg_minion.nix
@@ -1,11 +1,12 @@
 # pkgs/minion.nix
-{ symlinkJoin, makeWrapper, minion }:
+{ symlinkJoin, makeWrapper, minion, openjfx }:
 symlinkJoin {
   name = "minion-wrapped";
   paths = [ minion ];
   buildInputs = [ makeWrapper ];
   postBuild = ''
     wrapProgram $out/bin/minion \
+      --add-flags "--module-path ${openjfx}/lib --add-modules javafx.controls,javafx.fxml" \
       --set JAVA_TOOL_OPTIONS "-Dprism.lcdtext=false -Dprism.text=t2k"
   '';
 }
