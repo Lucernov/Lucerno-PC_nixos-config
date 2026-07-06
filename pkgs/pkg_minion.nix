f4cfc3f5ba59068bc3a3aa64c655a128a103e2b5
Lucernov <jin.riv@gmail.com>
2026-07-06 15:01:42 +0300

    pre-rebuild

--- pkgs/pkg_minion.nix
+++ pkgs/pkg_minion.nix
@@ -1,17 +1,9 @@
 # pkgs/minion.nix
-{ stdenv, minion, jre8 }:
+{ pkgs, minion, jre8 }:
 let
   jar = "${minion}/share/minion/Minion-jfx.jar";
 in
-stdenv.mkDerivation {
-  name = "minion-wrapped";
-  installPhase = ''
-    mkdir -p $out/bin
-    cat > $out/bin/minion << EOF
-    #!${stdenv.shell}
-    export JAVA_TOOL_OPTIONS="-Dprism.lcdtext=false -Dprism.text=t2k"
-    exec ${jre8}/bin/java -jar ${jar} "\$@"
-    EOF
-    chmod +x $out/bin/minion
-  '';
-}
+pkgs.writeShellScriptBin "minion" ''
+  export JAVA_TOOL_OPTIONS="-Dprism.lcdtext=false -Dprism.text=t2k"
+  exec ${jre8}/bin/java -jar ${jar} "$@"
+''
