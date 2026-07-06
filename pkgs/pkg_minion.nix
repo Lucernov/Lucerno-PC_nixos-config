70ba1469789b2e44b17357e1eb5a22ade0ce0bb6
Lucernov <jin.riv@gmail.com>
2026-07-06 16:23:18 +0300

    pre-rebuild

--- pkgs/pkg_minion.nix
+++
@@ -1,9 +0,0 @@
-# pkgs/minion.nix
-{ pkgs, minion, jre8 }:
-let
-  jar = "${minion}/share/minion/Minion-jfx.jar";
-in
-pkgs.writeShellScriptBin "minion" ''
-  export JAVA_TOOL_OPTIONS="-Dprism.lcdtext=false -Dprism.text=t2k"
-  exec ${jre8}/bin/java -jar ${jar} "$@"
-''
