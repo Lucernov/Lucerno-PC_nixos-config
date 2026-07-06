6c89a94d9aa626e7c51083ada11f2ba20b3323d5
Lucernov <jin.riv@gmail.com>
2026-07-06 17:08:44 +0300

    pre-rebuild

--- modules/packages.nix
+++ modules/packages.nix
@@ -103,7 +103,8 @@ let
     papers                                                      # Просмотрщик документов (PDF, DjVu, PostScript) — современная альтернатива Evince для GNOME
 
     # ИГРЫ
-    my-packages.minion                                          # Менеджер аддонов для TESO
+    minion
+    #my-packages.minion                                          # Менеджер аддонов для TESO
     (bottles.override { removeWarningPopup = true; })           # Запуск Windows-приложений через Wine (без всплывающих предупреждений)
     goverlay                                                    # Оверлей для мониторинга системы и FPS (MangoHud, vkBasalt)
     mangohud                                                    # Оверлей для отображения FPS и мониторинга системы в играх
