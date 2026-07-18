2ca45908608a2774dcc1a7fed5425eaa5ff118fc
Lucernov <jin.riv@gmail.com>
2026-07-18 13:59:58 +0300

    pre-rebuild

--- modules/nixos/nx_comfyui.nix
+++ modules/nixos/nx_comfyui.nix
@@ -1,18 +1,29 @@
 { pkgs, myLib, ... }:
 
 {
-  # Пользовательский systemd-сервис для запуска ComfyUI Сервис не запускается автоматически при входе в систему, а только по требованию или вручную systemctl --user start comfyui
   systemd.services.comfyui = {
-    description = "ComfyUI server (system)";                                                          # Описание сервиса (отображается в systemctl status)
+    description = "ComfyUI server (system)";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       User = myLib.userName;
       Group = myLib.userName;
-      WorkingDirectory = "/mnt/ai/ComfyUI";                                                            # Рабочая директория (где лежат модели и workflows)
-      ExecStart = "${pkgs.comfy-ui-cuda}/bin/comfy-ui --listen 127.0.0.1 --port 8188 --normalvram";    # Команда запуска - только локальный доступ
-      Restart = "on-failure";                                                                          # Перезапускать сервис, если он упал с ошибкой
-      RestartSec = 5;                                                                                  # Задержка перед перезапуском (5 секунд)
+      Type = "simple";
+      WorkingDirectory = "/mnt/ai/ComfyUI";
+      ExecStart = "${pkgs.comfy-ui-cuda}/bin/comfy-ui --listen 127.0.0.1 --port 8188 --normalvram";
+      Restart = "on-failure";
+      RestartSec = 5;
+      # Опции для доступа к устройствам и привилегиям (аналогично rclone)
+      PrivateDevices = false;
+      ProtectSystem = "off";
+      ProtectHome = false;
+      NoNewPrivileges = false;
+      PrivateMounts = false;
+      MountFlags = "shared";
+      SupplementaryGroups = [ "fuse" "render" "video" ]; # для доступа к GPU и FUSE
+      DeviceAllow = [ "/dev/fuse" "/dev/nvidia*" "/dev/dri/*" ];
+      DevicePolicy = "auto";
+      AmbientCapabilities = [ "CAP_SYS_ADMIN" ];
     };
   };
 }
