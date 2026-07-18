8caf4e92a3bb5c90225f50045eaf9fc33345bf3f
Lucernov <jin.riv@gmail.com>
2026-07-18 14:04:43 +0300

    pre-rebuild

--- modules/nixos/nx_comfyui.nix
+++ modules/nixos/nx_comfyui.nix
@@ -4,15 +4,17 @@
   systemd.services.comfyui = {
     description = "ComfyUI server (system)";
     after = [ "network.target" ];
-    # wantedBy = [ "multi-user.target" ];  # убираем, чтобы сервис не стартовал автоматически
+    # Раскомментируйте следующую строку, если нужна автозагрузка при старте системы
+    wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       User = myLib.userName;
       Group = myLib.userName;
       Type = "simple";
       WorkingDirectory = "/mnt/ai/ComfyUI";
-      ExecStart = "${pkgs.comfy-ui-cuda}/bin/comfy-ui --listen 127.0.0.1 --port 8188 --normalvram";
+      ExecStart = "${pkgs.comfy-ui-cuda}/bin/comfy-ui --listen 127.0.0.1 --port 8188";
       Restart = "on-failure";
       RestartSec = 5;
+      # Разрешаем доступ к устройствам и снимаем ограничения
       PrivateDevices = false;
       ProtectSystem = "off";
       ProtectHome = false;
