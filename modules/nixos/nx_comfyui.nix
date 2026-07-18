6909bb946250c8930f578a694afb926d968388e1
Lucernov <jin.riv@gmail.com>
2026-07-18 14:00:46 +0300

    pre-rebuild

--- modules/nixos/nx_comfyui.nix
+++ modules/nixos/nx_comfyui.nix
@@ -13,14 +13,13 @@
       ExecStart = "${pkgs.comfy-ui-cuda}/bin/comfy-ui --listen 127.0.0.1 --port 8188 --normalvram";
       Restart = "on-failure";
       RestartSec = 5;
-      # Опции для доступа к устройствам и привилегиям (аналогично rclone)
       PrivateDevices = false;
       ProtectSystem = "off";
       ProtectHome = false;
       NoNewPrivileges = false;
       PrivateMounts = false;
       MountFlags = "shared";
-      SupplementaryGroups = [ "fuse" "render" "video" ]; # для доступа к GPU и FUSE
+      SupplementaryGroups = [ "fuse" "render" "video" ]; # для доступа к GыPU и FUSE
       DeviceAllow = [ "/dev/fuse" "/dev/nvidia*" "/dev/dri/*" ];
       DevicePolicy = "auto";
       AmbientCapabilities = [ "CAP_SYS_ADMIN" ];
