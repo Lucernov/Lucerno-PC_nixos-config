09dc32a755c4a1f59a1de9cb42b03e94c97a5767
Lucernov <jin.riv@gmail.com>
2026-07-18 14:20:08 +0300

    pre-rebuild

--- modules/nixos/nx_comfyui.nix
+++ modules/nixos/nx_comfyui.nix
@@ -4,10 +4,10 @@
   systemd.services.comfyui = {
     description = "ComfyUI server (system)";
     after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];   # Автозапуск при загрузке
+    wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      # User = myLib.userName;            # временно закомментировано для теста от root
-      # Group = myLib.userName;
+      User = myLib.userName;
+      Group = myLib.userName;
       Type = "simple";
       WorkingDirectory = "/mnt/ai/ComfyUI";
       ExecStart = "${pkgs.comfy-ui-cuda}/bin/comfy-ui --listen 127.0.0.1 --port 8188";
@@ -16,6 +16,7 @@
       Environment = [
         "CUDA_VISIBLE_DEVICES=0"
         "LD_LIBRARY_PATH=/run/opengl-driver/lib:/run/opengl-driver/lib64"
+        "HOME=/home/lucerno"
       ];
       PrivateDevices = false;
       ProtectSystem = "off";
@@ -24,6 +25,16 @@
       PrivateMounts = false;
       MountFlags = "shared";
       SupplementaryGroups = [ "fuse" "render" "video" "nvidia" ];
+      DeviceAllow = [
+        "/dev/fuse"
+        "/dev/nvidia0"
+        "/dev/nvidiactl"
+        "/dev/nvidia-uvm"
+        "/dev/nvidia-uvm-tools"
+        "/dev/nvidia-modeset"
+        "/dev/dri/*"
+      ];
+      DevicePolicy = "closed";
       AmbientCapabilities = [ "CAP_SYS_ADMIN" ];
     };
   };
