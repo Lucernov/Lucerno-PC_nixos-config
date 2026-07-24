e38e1dc0969de608736bdef4edca4ac8c2bdb4f5
Lucernov <jin.riv@gmail.com>
2026-07-24 14:37:15 +0300

    pre-rebuild

--- modules/nx_obs.nix
+++ modules/nx_obs.nix
@@ -4,7 +4,8 @@
   programs.obs-studio = {
     enable = true;                                                 # Включает поддержку OBS
     package = pkgs.obs-studio.override { cudaSupport = true; };    # Основной пакет с CUDA
-    plugins = with pkgs.obs-studio-plugins; [                      # Плагины подключаются именно здесь
+    enableVirtualCamera = true;                                    # Включает виртуальную веб-камеру (v4l2loopback)
+    plugins = with pkgs.obs-studio-plugins; [                      # Подключение плагинов
       wlrobs                                                       # Захват экрана под Wayland
       obs-vaapi                                                    # Аппаратное кодирование через VA-API
       obs-pipewire-audio-capture                                   # Захват звука через PipeWire
