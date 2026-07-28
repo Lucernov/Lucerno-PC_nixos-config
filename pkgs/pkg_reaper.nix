df98c55b578f85a87b621a92f7a83086fccaaef4
Lucernov <jin.riv@gmail.com>
2026-07-28 05:00:46 +0300

    pre-rebuild

--- pkgs/pkg_reaper.nix
+++ pkgs/pkg_reaper.nix
@@ -6,13 +6,11 @@ symlinkJoin {
   paths = [ reaper ];
   buildInputs = [ stdenv.cc.cc.lib ];
   postBuild = ''
-    # Перемещаем оригинальный бинарник
     mv $out/bin/reaper $out/bin/.reaper-unwrapped
-    # Создаём новый скрипт-обёртку
     cat > $out/bin/reaper <<EOF
     #!/bin/sh
     export GDK_BACKEND=x11
-    export LD_LIBRARY_PATH=/run/opengl-driver/lib:${stdenv.cc.cc.lib}/lib:${xorg.libX11}/lib:${xorg.libxcb}/lib:${libxkbcommon}/lib:${glib}/lib:${cairo}/lib:${pango}/lib:${fontconfig}/lib
+    export LD_LIBRARY_PATH=/run/opengl-driver/lib:${stdenv.cc.cc.lib}/lib:${xorg.libX11}/lib:${xorg.libxcb}/lib:${xorg.libxcb-util}/lib:${xorg.libxcb-cursor}/lib:${xorg.libxcb-xkb}/lib:${libxkbcommon}/lib:${glib}/lib:${cairo}/lib:${pango}/lib:${fontconfig}/lib
     exec taskset -c 2-11 $out/bin/.reaper-unwrapped "\$@"
     EOF
     chmod +x $out/bin/reaper
