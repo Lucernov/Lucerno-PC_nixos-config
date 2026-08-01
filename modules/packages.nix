d1fab169e5675d895c6336e3085434533442599e
Lucernov <jin.riv@gmail.com>
2026-08-01 23:25:53 +0300

    2026-08-01 23:25:53

--- modules/packages.nix
+++ modules/packages.nix
@@ -187,8 +187,8 @@
     surge-xt                                                      # Синтезатор Surge XT
     my-packages.shortcircuit-xt                                   # Мощный открытый семплер (CLAP) от Surge Synth Team
     decent-sampler                                                # Сэмплер для библиотек DecentSampler (формат .dspreset, .dslibrary)
-    my-packages.orchestools
-    my-packages.ot-piano-s
+    my-packages.orchestools                                       # Набор оркестровых VST3-инструментов (Brass, Perc, Strings, Winds)
+    my-packages.ot-piano-s                                        # Пианино OT P1ANO S (VST2)
       # --- Синтезаторы и Сэмплеры ударных ---
     my-packages.mtpdk                                             # лёгкий плагин ударной установки MT-PowerDrumKit 2
     my-packages.drumlabooh                                        # LV2-сэмплер ударных с несколькими наборами (drumlabooh)
