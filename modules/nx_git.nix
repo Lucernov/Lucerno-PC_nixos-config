6c6556ed08074054dc8645290522118784f400e2
Lucernov <jin.riv@gmail.com>
2026-08-01 18:54:08 +0300

    pre-rebuild

--- modules/nx_git.nix
+++ modules/nx_git.nix
@@ -19,6 +19,13 @@ in
         hooksPath = ~/.git/hooks
       [credential]
         helper = store
+
+      # Настройки для Git LFS
+      [filter "lfs"]
+        process = git-lfs filter-process
+        required = true
+        clean = git-lfs clean -- %f
+        smudge = git-lfs smudge -- %f
     ''}"
 
     # Глобальный файл игнорирования Git (~/.gitignore)
