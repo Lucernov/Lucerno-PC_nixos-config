cbec51025948592b7bacde74a2bb4f9b8e6bf350
Lucernov <jin.riv@gmail.com>
2026-08-31 19:17:21 +0300

    pre-rebuild

--- modules/nx_kitty.nix
+++ modules/nx_kitty.nix
@@ -70,7 +70,7 @@ let
     # Прозрачность фона (0.95 = почти непрозрачный)
     background_opacity 0.1
     # Включает размытие фона, радиус размытия (можно менять от 0 до 50; чем больше, тем сильнее размытие)
-    background_blur 30
+    background_blur 1
     background_blur_opacity 0.1
     # Убрать рамку окна (закомментировано)
     # hide_window_decorations yes
