c2d54a4fc5185e670a60f997ea32d8cbac7078b8
Lucernov <jin.riv@gmail.com>
2026-09-03 09:30:22 +0300

    pre-rebuild

--- modules/nx_kate.nix
+++ modules/nx_kate.nix
@@ -2,18 +2,14 @@
 { myLib, ... }:
 
 let
-  # Корень ваших dotfiles для Kate
-  dotfilesKate = "${myLib.home}/${myLib.configDirName}/dotfiles/config/kate";
+
+  dotfilesKate = "${myLib.home}/${myLib.configDirName}/dotfiles/config/kate";                                             # Корень ваших dotfiles для Kate
 in
 {
-  systemd.tmpfiles.rules = [    # ---------- Симлинки для настроек Kate ----------
-    # Основной файл конфигурации Kate
-    "L+ ${myLib.home}/.config/katerc - lucerno lucerno - ${dotfilesKate}/katerc"
-    # Настройки плагина внешних инструментов (глобальные)
-    "L+ ${myLib.home}/.config/kate-externaltoolspluginrc - lucerno lucerno - ${dotfilesKate}/kate-externaltoolspluginrc"
-    # Настройки Vi-режима
-    "L+ ${myLib.home}/.config/katevirc - lucerno lucerno - ${dotfilesKate}/katevirc"
-    # Папка с внешними инструментами (рекурсивный симлинк) и файл настроек LSP-клиента .config/kate/lspclient/settings.json
-    "L+ ${myLib.home}/.config/kate - lucerno lucerno - ${dotfilesKate}/kate"
+  systemd.tmpfiles.rules = [                                                                                              # Симлинки для настроек Kate
+    "L+ ${myLib.home}/.config/katerc - lucerno lucerno - ${dotfilesKate}/katerc"                                          # Основной файл конфигурации Kate
+    "L+ ${myLib.home}/.config/kate-externaltoolspluginrc - lucerno lucerno - ${dotfilesKate}/kate-externaltoolspluginrc"  # Настройки плагина внешних инструментов (глобальные)
+    "L+ ${myLib.home}/.config/katevirc - lucerno lucerno - ${dotfilesKate}/katevirc"                                      # Настройки Vi-режима
+    "L+ ${myLib.home}/.config/kate - lucerno lucerno - ${dotfilesKate}/kate"                                              # Папка с внешними инструментами и настройки LSP-клиента .config/kate/lspclient/settings.json
   ];
 }
