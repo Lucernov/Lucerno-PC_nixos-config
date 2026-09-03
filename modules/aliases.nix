1371b0f32465566b0b56c089043bd83346128f9a
Lucernov <jin.riv@gmail.com>
2026-09-03 11:35:12 +0300

    pre-rebuild

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -25,7 +25,7 @@
       sync   = "cd ${myLib.home}/${myLib.configDirName} && git add -A && (git commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\" || true) && git push"; # синхронизировать конфиг с Git
       update = "cd ${myLib.home}/${myLib.configDirName} && git add -A && git commit -m \"pre-rebuild\" && git push && nh os switch";  # пересобрать NixOS без обновления входов
       upgrade = "cd ${myLib.home}/${myLib.configDirName} && git add -A && (git commit -m \"pre-upgrade: $(date '+%Y-%m-%d %H:%M:%S')\" || true) && git push && nh os switch --update && git add flake.lock && (git commit -m \"upgrade: $(date '+%Y-%m-%d %H:%M:%S')\" || true) && git push";  # пересобрать NixOS с обновлением flake.lock
-      clean   = "nh clean all --keep 2 && nh os boot --update";  # очистить старые поколения, обновить входы и переключиться
+      clean = "nh clean all --keep 2 && nh os switch";  # очистить старые поколения и переключиться
 
       # ========== Приложения ==========
       parabolic = "org.nickvision.tubeconverter";                # запустить Parabolic (загрузчик видео/аудио с YouTube)
