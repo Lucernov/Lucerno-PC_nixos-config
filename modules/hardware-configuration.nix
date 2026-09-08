68bcc09f83966da04b920729de1fcd3e5901ffef
Lucernov <jin.riv@gmail.com>
2026-09-08 16:24:59 +0300

    pre-rebuild

--- modules/hardware-configuration.nix
+++ modules/hardware-configuration.nix
@@ -153,7 +153,7 @@ in
 
   # ========== ЗАГРУЗКА И ЯДРО ==========
   boot = {
-    boot.kernelPackages = pkgsZen71.linuxPackages_zen;                      # Установка кастомного ZEN ядра
+    kernelPackages = pkgsZen71.linuxPackages_zen;                           # Установка кастомного ZEN ядра
   # kernelPackages = inputs.nix-cachyos-kernel.legacyPackages.${pkgs.stdenv.hostPlatform.system}."linuxPackages-cachyos-bore-lto-x86_64-v3";  # Установка кастомного CachyOS ядра для intel i5 13400f процессора
   # kernelPackages = pkgs.linuxPackages_zen;                                # Установка кастомного ZEN ядра
   # kernelPackages = pkgs.linuxPackages;                                    # Базовое ядро
