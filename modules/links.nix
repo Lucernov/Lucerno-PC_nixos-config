c652c8d4ac9393eaebb017f22dc2e1092d62d4e9
Lucernov <jin.riv@gmail.com>
2026-09-08 13:08:59 +0300

    pre-rebuild

--- modules/links.nix
+++ modules/links.nix
@@ -28,6 +28,7 @@ in
     "d ${home}/.config 0755 ${myLib.userName} ${myLib.userName} -"
     "d ${home}/.config/autostart 0755 ${myLib.userName} ${myLib.userName} -"
     "d ${home}/${configDir}/secrets 0750 ${myLib.userName} ${myLib.userName} -"
+    "d ${home}/.config/mc 0755 ${myLib.userName} ${myLib.userName} -"
 
     # ДИСКИ
     "d /mnt/ai 0755 ${myLib.userName} ${myLib.userName} -"
@@ -48,6 +49,7 @@ in
     "L+ ${home}/.config/kwinrc - ${myLib.userName} ${myLib.userName} - ${home}/${configDir}/dotfiles/config/KDE/config_kwinrc"
     "L+ ${home}/.config/kxkbrc - ${myLib.userName} ${myLib.userName} - ${home}/${configDir}/dotfiles/config/KDE/config_kxkbrc"
     "L+ ${home}/.config/plasmarc - ${myLib.userName} ${myLib.userName} - ${home}/${configDir}/dotfiles/config/KDE/config_plasmarc"
+    "L+ ${home}/.config/mc/ini - ${myLib.userName} ${myLib.userName} - ${home}/${configDir}/dotfiles/config/mc/ini"
 
 
     # ---------- Kdenlive ----------
