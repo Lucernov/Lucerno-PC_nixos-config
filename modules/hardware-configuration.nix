2e3a982eb15153d3a34ca891afe5595214cfa115
Lucernov <jin.riv@gmail.com>
2026-09-08 16:43:10 +0300

    pre-upgrade: 2026-09-08 16:43:10

--- modules/hardware-configuration.nix
+++ modules/hardware-configuration.nix
@@ -153,11 +153,11 @@ in
 
   # ========== ЗАГРУЗКА И ЯДРО ==========
   boot = {
-  # kernelPackages = pkgsZen71.linuxPackages_zen;                           # Установка кастомного ZEN ядра
+    kernelPackages = pkgsZen71.linuxPackages_zen;                           # Установка кастомного ии жестко зафиксированного на версии 7.1 ZEN ядра пока не починят дрова нвидиа для версиии 7.2
   # kernelPackages = inputs.nix-cachyos-kernel.legacyPackages.${pkgs.stdenv.hostPlatform.system}."linuxPackages-cachyos-bore-lto-x86_64-v3";  # Установка кастомного CachyOS ядра для intel i5 13400f процессора
   # kernelPackages = pkgs.linuxPackages_zen;                                # Установка кастомного ZEN ядра
   # kernelPackages = pkgs.linuxPackages;                                    # Базовое ядро
-    kernelPackages = pkgs.linuxPackages_latest;
+  # kernelPackages = pkgs.linuxPackages_latest;
 
     initrd.kernelModules = [                                                # Модули, загружаемые на раннем этапе (initrd)
       "nvidia"                                                              # Основной драйвер NVIDIA
