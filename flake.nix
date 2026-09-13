dba911425e41ec446b421d4bd9ee6c4000618c6f
Lucernov <jin.riv@gmail.com>
2026-09-13 12:09:39 +0300

    pre-upgrade: 2026-09-13 12:09:39

--- flake.nix
+++ flake.nix
@@ -41,8 +41,13 @@
       inputs.nixpkgs.follows = "nixpkgs";                                                                  # Зависимости используют основной nixpkgs
     };
 
+    comfyui-nix = {
+      url = "github:utensils/comfyui-nix";                                                                 # Flake для ComfyUI
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     import-tree.url = "github:vic/import-tree";                                                            # Утилита для рекурсивного импорта файлов
-    comfyui-nix.url = "github:utensils/comfyui-nix";                                                       # Flake для ComfyUI
+  # comfyui-nix.url = "github:utensils/comfyui-nix";                                                       # Flake для ComfyUI
     nixpkgs-krita-25-11.url = "github:NixOS/nixpkgs/b77b3de8775677f84492abe84635f87b0e153f0f";             # Фиксированная версия Krita (новая версия пока не работает с ComfyUI)
     nixpkgs-minion-25-11.url = "github:NixOS/nixpkgs/b77b3de8775677f84492abe84635f87b0e153f0f";            # Фиксированная версия minion, пакет в репозитории сломался из-за изменений в Яве. Пока чинят https://github.com/NixOS/nixpkgs/pull/539572 !!! TEMP !!!
 
