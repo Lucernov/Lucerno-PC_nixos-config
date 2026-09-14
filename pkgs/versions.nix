d422596de887fc8db9cb01714b8f22bf615feb7a
Lucernov <jin.riv@gmail.com>
2026-09-14 13:26:55 +0300

    pre-rebuild

--- pkgs/versions.nix
+++ pkgs/versions.nix
@@ -8,24 +8,24 @@
     urlVersion = "109";
   };
 
-  amp-locker = {                                                        # обновляется активно (Audio Assault)
-    version  = "1.5.6";
+  amp-locker = {                                                        # обновляется иногда
+    version  = "1.5.6";                                                 # https://audioassault.mx/downloadAudioAssault
     hash     = "sha256-pHh4SN6Vb7CFOOQD+9VFLlNch0lv4dU8EWXjSE68iv8=";
   };
 
-  drum-locker = {
-    version   = "1.0.2";
+  drum-locker = {                                                       # обновляется иногда
+    version   = "1.0.2";                                                # https://audioassault.mx/downloadAudioAssault
     hash      = "sha256-fX6k5C64wNlHK1QsrdClitWlFR33jymEdVO7QIVFNGs=";
   };
 
-  drumlabooh  = {
-    version   = "12.2.0";
+  drumlabooh  = {                                                       # обновляется иногда
+    version   = "12.2.0";                                               # https://github.com/psemiletov/drumlabooh/releases
     hash      = "sha256-IQ0XzIwJqGg+6FynmJBllyBIzWD3dgFfllOTEx0cMDM=";  # одноканальный
     hashMulti = "sha256-qdZJvXsUlEmmlTwUwO/C47OXM+gwRlu2cNRFGrJDi1A=";  # мультиканальный
   };
 
   je8086    = {                                                         # обновляется активно
-    version = "2.2.16";
+    version = "2.2.16";                                                 # https://theusualsuspects.io/builds/downloads?product=JE8086&format=All&os=Linux_x86_64
     hash    = "sha256-KoBRwHO2YJLl/bpAtzB/TPKYn2abiZ+hDaCjVqI+LFU=";
   };
 
@@ -40,7 +40,7 @@
   };
 
   ostirus   = {                                                         # обновляется активно
-    version = "2.2.16";
+    version = "2.2.16";                                                 # https://theusualsuspects.io/builds/downloads?product=OsTIrus&format=All&os=Linux_x86_64
     hash    = "sha256-+3g9yEOb2Psjj/K9ZIY6GXYeIwIsRCtIEOrcUZ980eY=";
   };
 
@@ -48,13 +48,13 @@
     version  = "1.0";
   };
 
-  sforzando = {
-    version = "1.982";
+  sforzando = {                                                         # обновляется иногда
+    version = "1.982";                                                  # https://www.plogue.com/downloads.html
     hash    = "sha256-7ms1T9N1/50M4wgZaD9E07cSof5P9Tx35E3wNtqCqQA=";
   };
 
-  shortcircuit-xt = {
-    version       = "2026-07-31-7d79b3a";
+  shortcircuit-xt = {                                                   # обновляется активно
+    version       = "2026-09-14-8cda0ce";                               # https://github.com/surge-synthesizer/shortcircuit-xt/releases/
     releaseTag    = "Nightly";
     hash          = "sha256-dbod6Bc7W2+ul0IUXFg9Olai75VhLAtXMobj3kgdklI=";
   };
