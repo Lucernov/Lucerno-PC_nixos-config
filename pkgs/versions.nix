a7b3a9565255a820cc4a72401f0d0bb78ac6f429
Lucernov <jin.riv@gmail.com>
2026-09-14 13:30:05 +0300

    pre-rebuild

--- pkgs/versions.nix
+++ pkgs/versions.nix
@@ -14,7 +14,7 @@
   };
 
   drum-locker = {                                                       # обновляется иногда
-    version   = "1.0.2";                                                # https://audioassault.mx/downloadAudioAssault
+    version   = "1.0.3";                                                # https://audioassault.mx/downloadAudioAssault
     hash      = "sha256-fX6k5C64wNlHK1QsrdClitWlFR33jymEdVO7QIVFNGs=";
   };
 
