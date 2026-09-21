e9c698731bfcc0d219c5256989510f9dc8d5b3d2
Lucernov <jin.riv@gmail.com>
2026-09-21 13:04:55 +0300

    pre-rebuild

--- flake.nix
+++ flake.nix
@@ -60,9 +60,9 @@
         localSystem = "x86_64-linux";                                                                      # Новый синтаксис с атрибутом localSystem вместо устаревшего `system`
         config = {
           allowUnfree = true;
-          cudaSupport = true;
-          cudaCapabilities = [ "8.6" ];
-          cudaForwardCompat = false;
+        # cudaSupport = true;                                                                              # Включаем поддержку CUDA глобально
+        # cudaCapabilities = [ "8.6" ];                                                                    # Только Ampere (RTX 3070). Не тратим время на sm_70, sm_75, sm_80, sm_89 и т.д.
+        # cudaForwardCompat = false;                                                                       # Не генерируем PTX для будущих архитектур – экономит ещё больше времени
         };
       };
 
@@ -75,9 +75,9 @@
         localSystem = "x86_64-linux";                                                                      # Здесь также используем localSystem
         config = {
           allowUnfree = true;                                                                              # Разрешает установку пакетов с несвободными лицензиями
-          cudaSupport = true;                                                                              # Включаем поддержку CUDA глобально
-          cudaCapabilities = [ "8.6" ];                                                                    # Только Ampere (RTX 3070). Не тратим время на sm_70, sm_75, sm_80, sm_89 и т.д.
-          cudaForwardCompat = false;                                                                       # Не генерируем PTX для будущих архитектур – экономит ещё больше времени
+        # cudaSupport = true;                                                                              # Включаем поддержку CUDA глобально
+        # cudaCapabilities = [ "8.6" ];                                                                    # Только Ampere (RTX 3070). Не тратим время на sm_70, sm_75, sm_80, sm_89 и т.д.
+        # cudaForwardCompat = false;                                                                       # Не генерируем PTX для будущих архитектур – экономит ещё больше времени
         };
         overlays = [
           (import ./pkgs/default.nix { pkgs-unstable = pkgsUnstable; })                                    # Подключаем оверлей с моими пакетами (my-packages)
