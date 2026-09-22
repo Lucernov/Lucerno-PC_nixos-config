ee68d659f2a7145e7765590dcd1f8ef28578d89f
Lucernov <jin.riv@gmail.com>
2026-09-22 16:16:03 +0300

    pre-rebuild

--- modules/hardware-configuration.nix
+++ modules/hardware-configuration.nix
@@ -309,6 +309,7 @@ in
     usbmuxd = {
       enable = true;                                                        # Включаем демон для связи с iPhone по USB
       package = pkgs.usbmuxd2;                                              # Используем альтернативную реализацию usbmuxd2 (лучше совместима с новыми версиями iOS, чем стандартный usbmuxd)
+      environment.USBMUXD_DEFAULT_DEVICE_MODE = "1";                        # Принудительно использовать USB-режим 1 (ipheth), необходимый для раздачи интернета
     };
     fwupd.enable = true;                                                    # Включает демон fwupd для автоматического обновления прошивок устройств (UEFI, USB, диски и др.)
     xserver.videoDrivers = [ "nvidia" ];                                    # Использовать проприетарный драйвер NVIDIA (NVIDIA RTX 3070)
