74fe94768afd70516384026ac0e3a05eb7dd539d
Lucernov <jin.riv@gmail.com>
2026-09-23 14:20:06 +0300

    pre-upgrade: 2026-09-23 14:20:06

--- modules/default.nix
+++ modules/default.nix
@@ -112,7 +112,7 @@
         "https://cache.nixos-cuda.org"                                                                      # Специализированный CUDA-кэш
         "https://cache.flox.dev"                                                                            # Официальный кэш NVIDIA/CUDA (предотвращает компиляцию CUDA из исходников)
         "https://nix-community.cachix.org"                                                                  # Кэш Nix Community
-       #"https://adithyagenie.cachix.org"                                                                   # Кэш готовых сборок Blender с CUDA (от adithyagenie, экономит время компиляции)
+        "https://adithyagenie.cachix.org"                                                                   # Кэш готовых сборок Blender с CUDA (от adithyagenie, экономит время компиляции)
         "https://chaotic-nyx.cachix.org"                                                                    # Дополнительный кеш с большим количеством популярных пакетов
         "https://devenv.cachix.org"                                                                         # Кеш для сред разработки (devenv)
         "https://attic.xuyh0120.win/lantian"                                                                # Добавляем кэш аттика от xuyh0120 (содержит множество готовых сборок для CachyOS пакетов)
@@ -123,7 +123,7 @@
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="                                 # Публичный ключ CUDA-кэша
         "cache.flox.dev-1:H4Tsx+8AOz3b3CvyCqVQPyEr2cHKH+O8bHn8ZgYp/po="                                     # Ключ кэша flox (CUDA)
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="                           # ключ кэша nix-community
-       #"adithyagenie.cachix.org-1:h6BSMboeVfxyrULWuRQqAyweo4AJRATekb88xotfQwc="                            # Публичный ключ кэша adithyagenie (Blender с CUDA)
+        "adithyagenie.cachix.org-1:h6BSMboeVfxyrULWuRQqAyweo4AJRATekb88xotfQwc="                            # Публичный ключ кэша adithyagenie (Blender с CUDA)
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="                             # Ключ chaotic-nyx
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="                                  # Ключ devenv
         "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="                                              # Публичный ключ для проверки подписей пакетов из указанного аттика.
