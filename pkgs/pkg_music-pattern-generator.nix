21ee42eedde07a1768034698281a3c6ea19f9c64
Lucernov <jin.riv@gmail.com>
2026-09-25 18:57:59 +0300

    pre-rebuild

--- pkgs/pkg_music-pattern-generator.nix
+++ pkgs/pkg_music-pattern-generator.nix
@@ -133,8 +133,8 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     makeWrapper $out/opt/music-pattern-generator/nw $out/bin/mpg \
       --chdir "$out/opt/music-pattern-generator" \
-      --add-flags "--ozone-platform-hint=auto" \
-      --add-flags "--enable-features=WaylandWindowDecorations" \
+      --add-flags "--enable-features=UseOzonePlatform,WaylandWindowDecorations" \
+      --add-flags "--ozone-platform=wayland" \
       --prefix LD_LIBRARY_PATH : "$out/opt/music-pattern-generator/lib:${lib.makeLibraryPath [
         alsa-lib libX11 libXext libxcb libXcomposite libXdamage libXfixes
         libXrandr libXcursor libXi libXrender libXtst libXScrnSaver
