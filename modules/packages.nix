9d7cb45cd0ff07992a95917a0b44fb52ffdd2878
Lucernov <jin.riv@gmail.com>
2026-09-26 04:15:16 +0300

    pre-rebuild

--- modules/packages.nix
+++ modules/packages.nix
@@ -105,8 +105,8 @@
     file                                                          # Определение типа файла (ELF, PNG, tar, ...)
     libimobiledevice                                              # Библиотека и набор утилит для связи с устройствами Apple (iPhone, iPad) по USB
     ifuse                                                         # Утилита для монтирования файловой системы iPhone/iPad как обычной папки в Linux (через FUSE)
-    rtcqs                                                         # Real-Time Config Quick Scan – диагностика системы для аудио
     cpupower                                                      # Утилита для управления частотой CPU (используется для смены governor)
+  # rtcqs                                                         # Real-Time Config Quick Scan – диагностика системы для аудио (пока нет в NIXOS)
 
     # ========== КОНСОЛЬНЫЕ УТИЛИТЫ ==========
     kitty                                                         # Эмулятор терминала с поддержкой GPU и лигатур
